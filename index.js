d4ffb76a105c3bec5f32f5c05cc6f4b0cb12d6a6
razzaksr <razzaksr@gmail.com>
2024-10-24 17:06:36 +0530

    chnages in index

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const parser = require('body-parser')
 
 const exp=express()
 
-exp.get('/',async(req,res)=>{
+exp.get('/',(req,res)=>{
     res.send({"hai":"Hello"})
 })
 
-exp.listen('1234',async(req,res)=>{
+exp.listen('1234',(req,res)=>{
     console.log('Server started')
 })
